1175b685bd2a6fa236f0f0db8078a771c3ee7172
chamathabeysinghe <abeysinghechamath@gmail.com>
2017-12-10 16:42:48 +0530

    Initial

---
+++ my_model_test.py
@@ -0,0 +1,65 @@
+import numpy as np
+import argparse
+import cv2
+
+# construct the argument parse and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", required=True, help="path to input image")
+ap.add_argument("-c", "--confidence", type=float, default=0.9, help="minimum probability to filter weak detections")
+args = vars(ap.parse_args())
+
+# initialize the list of class labels MobileNet SSD was trained to
+# detect, then generate a set of bounding box colors for each class
+CLASSES = ["background", "hat"]
+COLORS = np.random.uniform(0, 255, size=(len(CLASSES), 3))
+
+# load our serialized model from disk
+print("[INFO] loading model...")
+net = cv2.dnn.readNetFromTensorflow("hat_model/frozen_inference_graph.pb", "hat_model/ssd_mobilenet_v1_coco.pbtxt")
+# net = cv2.dnn.readNetFromTensorflow("ssd_mobilenet_v1_coco_11_06_2017/frozen_inference_graph.pb", "ssd_mobilenet_v1_coco_11_06_2017/ssd_mobilenet_v1_coco.pbtxt")
+#
+
+# load the input image and construct an input blob for the image
+# by resizing to a fixed 300x300 pixels and then normalizing it
+# (note: normalization is done via the authors of the MobileNet SSD
+# implementation)
+image = cv2.imread(args["image"])
+(h, w) = image.shape[:2]
+blob = cv2.dnn.blobFromImage(cv2.resize(image, (300, 300)), 0.007843, (300, 300), 127.5)
+
+print("[INFO] computing object detections...")
+net.setInput(blob)
+detections = net.forward()
+
+print "[INFO] Done detection"
+
+
+# loop over the detections
+for i in np.arange(0, detections.shape[2]):
+    # extract the confidence (i.e., probability) associated with the
+    # prediction
+    confidence = detections[0, 0, i, 2]
+
+    # filter out weak detections by ensuring the `confidence` is
+    # greater than the minimum confidence
+    if confidence > args["confidence"]:
+        # extract the index of the class label from the `detections`,
+        # then compute the (x, y)-coordinates of the bounding box for
+        # the object
+        idx = int(detections[0, 0, i, 1])
+        box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+        (startX, startY, endX, endY) = box.astype("int")
+
+        # display the prediction
+        label = "{}: {:.2f}%".format(CLASSES[idx], confidence * 100)
+        print("[INFO] {}".format(label))
+        cv2.rectangle(image, (startX, startY), (endX, endY), COLORS[idx], 2)
+        y = startY - 15 if startY - 15 > 15 else startY + 15
+        cv2.putText(image, label, (startX, y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, COLORS[idx], 2)
+
+cv2.imshow("Output", image)
+cv2.waitKey(0)
+
+
+
+# python train.py --prototxt MobileNetSSD_deploy.prototxt.txt --model MobileNetSSD_deploy.caffemodel --image images/example_01.jpg
